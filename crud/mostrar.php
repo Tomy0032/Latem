b7efa58c00aa8dd8053edf7ea6af7a0870de2c8a
Tomy0032 <tomas.com9@gmail.com>
2021-09-19 15:59:04 -0300

    Se integran las funciones de agregar y modificar productos dentro de la página del crud. Se eliminan las páginas obsoletas

--- crud/mostrar.php
+++ crud/mostrar.php
@@ -8,9 +8,32 @@ require_once('categoria.php');
 $db=Db::conectar();
 $crud=new CrudProducto();
 $producto= new producto();
-
 //obtiene todos los libros con el método mostrar de la clase crud
 $listaProductos=$crud->mostrar();
+
+
+if (isset ($_GET['accion']) && $_GET['accion'] == 'a') {
+
+	?>
+	<style>
+		#ingProd{
+		 	display: none;
+		 }
+	</style>
+	<?php
+	//busca el producto utilizando el id, que es enviado por GET desde la vista mostrar.php
+	$producto=$crud->obtenerProducto($_GET['id']);
+
+}else{
+	?>
+	<style>
+		#actProd{
+		 	display: none;
+		 }
+	</style>
+	<?php
+}
+
 ?>
  
 
@@ -28,24 +51,28 @@ $listaProductos=$crud->mostrar();
 	<link rel="stylesheet" href="/utu/Latem/recursos/iconos/css/all.min.css">
 </head>
 <body>
-
 	<header>	
 			<!--=====================================
 			=          Barra de navegación          =
 			======================================-->
 			
 			<div id="menu">
+
+				<!---Logo--->
 				<a href="">
 					<img src="/utu/Latem/recursos/RoboTech logo.png" alt="">
 				</a>
-				<form action="">
-					<div id="buscador">
-						<input type="text" class="a" placeholder="Buscar" required>
-						<button type="submit" id="btn-buscar">
-							<i class="fas fa-search"></i>
-						</button>
-					</div>
+				<!---Logo--->
+
+				<!---Buscador--->
+				<form action="" id="buscador">
+					<input type="text" placeholder="Buscar" required>
+					<button type="submit" id="btn-buscar">
+						<i class="fas fa-search"></i>
+					</button>
 				</form>
+				<!---Buscador--->
+
 				<nav>
 					<ul>
 						<li id="productos">
@@ -53,7 +80,76 @@ $listaProductos=$crud->mostrar();
 								Productos
 							</button>
 							<div id="menu-productos">
-								
+								<div id="categorias">
+									<div >
+										<h4>Robótica</h4>
+										<ul>
+											<li>
+												<a href="">Tarjetas de desarrollo</a>
+											</li>
+											<li>
+												<a href="">Módulos</a>
+											</li>
+											<li>
+												<a href="">Accesorios</a>
+											</li>
+											<li>
+												<a href="">Fuentes de alimentación</a>
+											</li>
+										</ul>
+									</div>
+									<div>
+										<h4>Componentes</h4>
+										<ul>
+											<li>
+												<a href="">Diodos y Tristores</a>
+											</li>
+											<li>
+												<a href="">Cables y Conectores</a>
+											</li>
+											<li>
+												<a href="">Transistores</a>
+											</li>
+											<li>
+												<a href="">Interruptores y Reles</a>
+											</li>
+											<li>
+												<a href="">Resistivos</a>
+											</li>
+										</ul>
+									</div>
+									<div>
+										<h4>Instrumentos</h4>
+										<ul>
+											<li>
+												<a href="">Soldadores y Desoldadores</a>
+											</li>
+											<li>
+												<a href="">Equipamiento antiestático</a>
+											</li>
+											<li>
+												<a href="">Medidores</a>
+											</li>
+										</ul>
+									</div>
+									<div>
+										<h4>Sensores</h4>
+										<ul>
+											<li>
+												<a href="">Sonido</a>
+											</li>
+											<li>
+												<a href="">Humedad</a>
+											</li>
+											<li>
+												<a href="">Luminosidad</a>
+											</li>
+											<li>
+												<a href="">Temperatura</a>
+											</li>
+										</ul>
+									</div>
+								</div>
 							</div>
 						</li>
 						<li>
@@ -64,12 +160,12 @@ $listaProductos=$crud->mostrar();
 						</li>
 						<li>
 							<a href="login/login.html" class="icon">
-								<i class="fas fa-sign-in-alt"></i>
+								<i class="fas fa-user"></i>
 							</a>
 						</li>
 						<li>
-							<a href="ingresar.php" class="icon">
-								<i class="fas fa-plus"></i>
+							<a href="" class="icon">
+								<i class="fas fa-shopping-cart"></i>
 							</a>
 						</li>
 					</ul>
@@ -79,43 +175,205 @@ $listaProductos=$crud->mostrar();
 			<!--====  End of Barra de navegación  ====-->
 	</header>
 
-	<!--=====================================
-	=            Mostrar productos            =
-	======================================-->
+	<div id="crud">
+		<div id="ingProd">
+			<h2>Ingrese los datos del producto</h2>
 
-	<table id="mostrar">
-		<thead>
-			<td>ID</td>
-			<td>Categoría</td>
-			<td>IVA</td>
-			<td>Proveedor</td>
-			<td>Nombre</td>
-			<td>Descripción</td>
-			<td>Precio</td>
-			<td>Stock</td>
-			<td>Modificar</td>
-			<td>Eliminar</td>
-		</thead>
-		<tbody>
-			<?php foreach ($listaProductos as $producto) {?>
-			<tr>
-				<td><?php echo $producto->getId() ?></td>
-				<td><?php echo $producto->getId_categoria() ?></td>
-				<td><?php echo $producto->getId_iva() ?></td>
-				<td><?php echo $producto->getId_proveedor() ?></td>
-				<td><?php echo $producto->getNombre() ?></td>
-				<td><?php echo $producto->getDescripcion() ?></td>
-				<td><?php echo $producto->getPrecio()?> </td>
-				<td><?php echo $producto->getStock() ?></td>
-				<td  class="icon"><a  href="actualizar.php?id=<?php echo $producto->getId()?>&accion=a"><i class="fas fa-edit"></i></a> </td>
-				<td  class="icon"><a  href="administrar_producto.php?id=<?php echo $producto->getId()?>&accion=e"><i class="fas fa-trash"></i></a>   </td>
-			</tr>
-			<?php }?>
-		</tbody>
-	</table>
-	
-	
-	<!--====  End of Mostrar productos  ====-->
-	
+			<!--=========================================
+			=            Formulario ingresar            =
+			==========================================-->
+
+			<form action='administrar_producto.php' method='post'>
+
+				<input type="hidden" name="id">
+				<p>
+					<label for="nombre">
+						Nombre Producto:
+						<input type='text' name='nombre' required>
+					</label>
+				</p>
+				<p>
+					<label for="id_categoria">
+						Categoría:
+						<select name="id_categoria" required>
+						    <option value="1">Robótica</option>
+							<option value="2">Instrumentos</option>
+							<option value="3">Componentes</option>
+						</select>
+					</label>
+				</p>
+				<p>
+					<label for="descripcion">
+						Descripción:
+						<textarea name="descripcion" required></textarea >
+					</label>	
+				</p>
+				<p>
+					<label for="precio">
+						Precio:
+						<input type="number" name="precio" required>
+					</label>
+				</p>
+				<p>
+					<label for="id_iva">
+						IVA:
+						<select name="id_iva" required>
+							<option value="1">Tipo 1</option>
+							<option value="2">Tipo 2</option>
+							<option value="3">Tipo 3</option>
+						</select>
+					</label>
+				</p>
+				<p>
+					<label for="id_proveedor">
+						Proveedor:
+						<select name="id_proveedor" required>
+							<option value="1">Proveedor A</option>
+							<option value="2">Proveedor B</option>
+							<option value="3">Proveedor C</option>
+							<option value="4">Proveedor D</option>
+						</select>
+					</label>
+				</p>
+				<p>
+					<label for="stock">
+						Cantidad:
+						<input type="number" name="stock" required>
+					</label>
+				</p>	
+				<p>
+					<input type='hidden' name='insertar' value='insertar'>
+					<button type="submit" id="btn-ingProd">
+						<i class="fas fa-save"></i>
+					</button>
+				</p>				
+			</form> 
+
+			<!--====  End of Formulario ingresar  ====-->
+			
+		</div>
+
+		<div id="actProd">
+			<h2>Actualizar los datos del producto</h2>
+
+			<!--==========================================
+			=            Formulario modificar            =
+			===========================================-->
+
+			<form action='administrar_producto.php' method='post'>
+
+				<input type='hidden' name='id' value='<?php echo $producto->getId()?>'>
+				<p>
+					<label for="nombre">
+						Nombre del Producto:
+						<input type='text' name='nombre' value='<?php echo $producto->getNombre()?>' required>
+					</label>
+				</p>
+				<p>
+					<label for="id_categoria">
+						Categoría:
+						<select name="id_categoria" required>
+			                <option value="1">Robótica</option>
+			                <option value="2">Instrumentos</option>
+			                <option value="3">Componentes</option>
+	                	</select>
+					</label>
+				</p>
+				<p>
+					<label for="descripcion">
+						Descripción:
+						<textarea name="descripcion" required><?php echo $producto->getDescripcion()?></textarea >
+					</label>
+				</p>
+				<p>
+					<label for="precio">
+						Precio:
+						<input type="number" name="precio" value='<?php echo $producto->getPrecio()?>' required>
+					</label>
+				</p>
+				<p>
+					<label for="id_iva">
+						IVA:
+						<select name="id_iva" required>
+							<option value="1">Tipo 1</option>
+							<option value="2">Tipo 2</option>
+							<option value="3">Tipo 3</option>
+						</select>
+					</label>
+				</p>
+				<p>
+					<label for="id_proveedor">
+						Proveedor:
+						<select name="id_proveedor" required>
+							<option value="1">Proveedor A</option>
+							<option value="2">Proveedor B</option>
+							<option value="3">Proveedor C</option>
+							<option value="4">Proveedor D</option>
+						</select>
+					</label>
+				</p>
+				<p>
+					<label for="stock">
+						Stock:
+						<input type="number" name="stock" value='<?php echo $producto->getStock()?>' required>
+					</label>
+				</p>
+						
+				<input type='hidden' name='actualizar' value='actualizar'>
+			<p>
+				<button type="submit">
+						<i class="fas fa-save"></i>
+				</button>
+				<button>
+						<a href="mostrar.php">
+							<i class="fas fa-times"></i>
+						</a>
+				</button>
+			</p>			
+		</form>
+
+		<!--====  End of Formulario modificar  ====-->
+		
+	</div>
+
+		<div id="mostProd">
+			<!--=====================================
+			=            Mostrar productos            =
+			======================================-->
+
+			<table id="mostrar">
+				<thead>
+					<td>ID</td>
+					<td>Categoría</td>
+					<td>IVA</td>
+					<td>Proveedor</td>
+					<td>Nombre</td>
+					<td>Descripción</td>
+					<td>Precio</td>
+					<td>Stock</td>
+					<td>Modificar</td>
+					<td>Eliminar</td>
+				</thead>
+				<tbody>
+					<?php foreach ($listaProductos as $producto) {?>
+					<tr>
+						<td><?php echo $producto->getId() ?></td>
+						<td><?php echo $producto->getId_categoria() ?></td>
+						<td><?php echo $producto->getId_iva() ?></td>
+						<td><?php echo $producto->getId_proveedor() ?></td>
+						<td><?php echo $producto->getNombre() ?></td>
+						<td><?php echo $producto->getDescripcion() ?></td>
+						<td><?php echo $producto->getPrecio()?> </td>
+						<td><?php echo $producto->getStock() ?></td>
+						<td  class="icon"><a  href="mostrar.php?id=<?php echo $producto->getId()?>&accion=a"><i class="fas fa-edit"></i></a> </td>
+						<td  class="icon"><a  href="administrar_producto.php?id=<?php echo $producto->getId()?>&accion=e"><i class="fas fa-trash"></i></a>   </td>
+					</tr>
+					<?php }?>
+				</tbody>
+			</table>
+			
+			<!--====  End of Mostrar productos  ====-->
+		</div>
+	</div>
 </body>
 </html>
